bc35aa1050a0dad176a046d593375f0710c98b80
weihuacern <hua.wei@email.ucr.edu>
2018-10-28 19:48:19 -0700

    first commit, works aok

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # react_pokemondex
+```
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+```
+
+```
+npm install
+npm start
+```
---
+++ src/components/search.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Search = ({ onChange }) => (
+  <input type="text" onChange={onChange} placeholder="Enter pokemon name..." />
+)
+
+export default Search
